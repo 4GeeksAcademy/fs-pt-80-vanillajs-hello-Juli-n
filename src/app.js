af3de9222948fb2196dd15e26a99de8ed8bf3f88
Julutus <julian94info@gmail.com>
2024-10-07 09:03:59 +0000

    Corregido inner.HTML

--- src/app.js
+++ src/app.js
@@ -7,26 +7,25 @@ import "./assets/img/4geeks.ico";
 
 const excuse = document.querySelector("#excuse");
 
+inner;
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-  excuse.innerHTML = generateExcuse();
+  const arr = ["The dog", "My grandma", "The mailman", "My bird"];
+  const arra = ["ate", "peed", "crushed", "broke"];
+  const arrb = ["my homework", "my phone", "the car"];
+  const arrc = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const getRandom = arr_y => Math.floor(Math.random() * arr_y.length);
+  const getValue = arr_y => arr_y[getRandom(arr_y)];
+
+  console.log(
+    `${getValue(arr)} ${getValue(arra)} ${getValue(arrb)} ${getValue(arrc)}`
+  );
 };
-
-const arr = ["The dog", "My grandma", "The mailman", "My bird"];
-const arra = ["ate", "peed", "crushed", "broke"];
-const arrb = ["my homework", "my phone", "the car"];
-const arrc = [
-  "before the class",
-  "when I was sleeping",
-  "while I was exercising",
-  "during my lunch",
-  "while I was praying"
-];
-
-const getRandom = arr_y => Math.floor(Math.random() * arr_y.length);
-const getValue = arr_y => arr_y[getRandom(arr_y)];
-
-console.log(
-  `${getValue(arr)} ${getValue(arra)} ${getValue(arrb)} ${getValue(arrc)}`
-);
